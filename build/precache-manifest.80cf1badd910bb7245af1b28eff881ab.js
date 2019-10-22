b0af254b1314b15aba61ccf1bbea3ed5c5555f90
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2019-10-22 12:00:39 +0300

    build

---
+++ build/precache-manifest.80cf1badd910bb7245af1b28eff881ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a804edd2845c930a5a21377e5553cce",
+    "url": "/auth-app/build/index.html"
+  },
+  {
+    "revision": "cb5dd29f2961bce0223b",
+    "url": "/auth-app/build/static/css/main.b95c5719.chunk.css"
+  },
+  {
+    "revision": "784bb023a19cfc1dc86d",
+    "url": "/auth-app/build/static/js/2.654a0c57.chunk.js"
+  },
+  {
+    "revision": "cb5dd29f2961bce0223b",
+    "url": "/auth-app/build/static/js/main.bad39add.chunk.js"
+  },
+  {
+    "revision": "2a686e2639a06a4f4819",
+    "url": "/auth-app/build/static/js/runtime-main.c7f6201c.js"
+  }
+]);
